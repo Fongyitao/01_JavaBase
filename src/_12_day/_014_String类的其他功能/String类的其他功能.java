bc4a765498b7bef46ff375a225f11908ffbecc9d
Fongyitao <2059634680@qq.com>
2017-09-25 23:34:40 +0800

    conmmit

---
+++ src/_12_day/_014_String类的其他功能/String类的其他功能.java
@@ -0,0 +1,17 @@
+package _12_day._014_String类的其他功能;
+
+public class String类的其他功能 {
+    /**
+     String 的替换功能
+        String replace(char old,char new)
+        String replace(String old,String new)
+
+     String的去除字符串两空格及案例
+        String trim()
+
+     String的按字典顺序比较两个字符串及案例演示
+        int compareTo(String str)
+        int compareToIgnoreCase(String str)
+     */
+
+}
