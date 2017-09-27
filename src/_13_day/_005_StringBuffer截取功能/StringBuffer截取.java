d3bb7dd4365a9fa6d8496ef4fb729f77597859eb
Fongyitao <2059634680@qq.com>
2017-09-27 21:46:54 +0800

    learn

--- src/_13_day/_005_StringBuffer截取功能/StringBuffer截取.java
+++ src/_13_day/_005_StringBuffer截取功能/StringBuffer截取.java
@@ -8,12 +8,16 @@ public class StringBuffer截取 {
     * public String substring(int start,int end):
         * 截取从指定位置开始到结束位置，包括开始位置，不包括结束位置
 * B:注意事项
-    * 注意:返回值类型不再是StringBuffer本身
+    * 注意:返回值类型不再是StringBuffer本身，而是String类型
 
      */
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("HelloWorld");
 
-        System.out.println(sb.substring(5));
+        String s1 = sb.substring(5);// 截取，包含头，不包含尾
+        System.out.println(s1); // World
+
+        String s2 = sb.substring(5,6);
+        System.out.println(s2);
     }
 }
