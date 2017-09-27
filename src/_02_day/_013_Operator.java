5d970c692b70992d18a15d1fd37536401b2fb988
Fongyitao <2059634680@qq.com>
2017-09-27 19:55:27 +0800

    learn

---
+++ src/_02_day/_013_Operator.java
@@ -0,0 +1,12 @@
+package _02_day;
+
+public class _013_Operator {
+    public static void main(String[] args) {
+        //关系运算符
+        //	==, != , > ,>= , < , <=
+        System.out.println(3 == 4);//	false
+        System.out.println(3 != 4);//	true
+        System.out.println(3 <= 4);//	true
+
+    }
+}
