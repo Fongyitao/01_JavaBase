a2227eafedb4def6948b3e4474f8a58479d4b794
Fongyitao <2059634680@qq.com>
2017-09-29 22:55:20 +0800

    learn

---
+++ src/_14_day/_001_正则表达式概述/_04_正则_数量词.java
@@ -0,0 +1,17 @@
+package _14_day._001_正则表达式概述;
+
+public class _04_正则_数量词 {
+    /*
+* A:Greedy 数量词
+    * X? X，一次或一次也没有
+    * X* X，零次或多次
+    * X+ X，一次或多次
+    * X{n} X，恰好 n 次
+    * X{n,} X，至少 n 次
+    * X{n,m} X，至少 n 次，但是不超过 m 次
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
