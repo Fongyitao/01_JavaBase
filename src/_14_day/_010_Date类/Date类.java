a0248312a1e6a0d5683721f93b3783b41d34e0de
Fongyitao <2059634680@qq.com>
2017-10-02 21:11:18 +0800

    learn

---
+++ src/_14_day/_010_Date类/Date类.java
@@ -0,0 +1,18 @@
+package _14_day._010_Date类;
+
+public class Date类 {
+    /*
+* A:Date类的概述
+    * 类 Date 表示特定的瞬间，精确到毫秒。
+* B:构造方法
+    * public Date()
+    * public Date(long date)
+* C:成员方法
+    * public long getTime()
+    * public void setTime(long time)
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
