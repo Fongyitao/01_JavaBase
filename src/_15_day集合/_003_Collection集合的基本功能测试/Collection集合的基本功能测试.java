f857159291a72cf1261a4b7d7f8147a2d2a46828
Fongyitao <2059634680@qq.com>
2017-10-03 00:35:53 +0800

    learn

---
+++ src/_15_day集合/_003_Collection集合的基本功能测试/Collection集合的基本功能测试.java
@@ -0,0 +1,26 @@
+package _15_day集合._003_Collection集合的基本功能测试;
+
+public class Collection集合的基本功能测试 {
+    /*
+* A:案例演示
+*
+        基本功能演示
+
+        boolean add(E e)
+        boolean remove(Object o)
+        void clear()
+        boolean contains(Object o)
+        boolean isEmpty()
+        int size()
+
+* B:注意:
+*
+        collectionXxx.java使用了未经检查或不安全的操作.
+        注意:要了解详细信息,请使用 -Xlint:unchecked重新编译.
+        java编译器认为该程序存在安全隐患
+        温馨提示:这不是编译失败,所以先不用理会,等学了泛型你就知道了
+     */
+    public static void main(String[] args) {
+
+    }
+}
