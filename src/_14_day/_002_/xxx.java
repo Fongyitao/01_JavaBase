ca1a9997a0152d5371a304eb8ac06b16179cb801
Fongyitao <2059634680@qq.com>
2017-10-02 15:53:41 +0800

    learn

--- src/_14_day/_002_/xxx.java
+++ src/_14_day/_002_/xxx.java
@@ -1,7 +1,11 @@
 package _14_day._002_;
 
+import java.math.BigDecimal;
+
 public class xxx {
     public static void main(String[] args) {
-        System.out.println("");
+        BigDecimal bd = new BigDecimal(1/3);
+        System.out.println(bd);
+
     }
 }
