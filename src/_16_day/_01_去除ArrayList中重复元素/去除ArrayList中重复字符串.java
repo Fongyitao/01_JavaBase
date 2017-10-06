69dcef3ccc4aa714a2da2fb0d69313f1fc518c85
Fongyitao <2059634680@qq.com>
2017-10-06 18:37:00 +0800

    learn

---
+++ src/_16_day/_01_去除ArrayList中重复元素/去除ArrayList中重复字符串.java
@@ -0,0 +1,13 @@
+package _16_day._01_去除ArrayList中重复元素;
+
+public class 去除ArrayList中重复字符串 {
+    /*
+* A:案例演示
+    * 需求：ArrayList去除集合中字符串的重复值(字符串的内容相同)
+    * 思路：创建新集合方式
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/_16_day/_02_LinkedList的特有功能/LinkedList的特有功能.java
@@ -0,0 +1,7 @@
+package _16_day._02_LinkedList的特有功能;
+
+public class LinkedList的特有功能 {
+    public static void main(String[] args) {
+
+    }
+}
