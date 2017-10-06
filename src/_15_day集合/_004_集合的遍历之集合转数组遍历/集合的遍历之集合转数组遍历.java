010dcd61c26f49ad95bba54bd589f975aa074fcc
Fongyitao <2059634680@qq.com>
2017-10-06 15:17:50 +0800

    learn

--- src/_15_day集合/_004_集合的遍历之集合转数组遍历/集合的遍历之集合转数组遍历.java
+++ src/_15_day集合/_004_集合的遍历之集合转数组遍历/集合的遍历之集合转数组遍历.java
@@ -1,5 +1,10 @@
 package _15_day集合._004_集合的遍历之集合转数组遍历;
 
+import _15_day集合._001_对象数组概述和使用.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class 集合的遍历之集合转数组遍历 {
     /*
 * A:集合的遍历
@@ -21,5 +26,16 @@ public class 集合的遍历之集合转数组遍历 {
      */
     public static void main(String[] args) {
 
+        Collection coll = new ArrayList();
+        coll.add(new Student("张三", 23));        //Object obj = new Student("张三",23);
+        coll.add(new Student("李四", 24));
+        coll.add(new Student("王五", 25));
+        coll.add(new Student("赵六", 26));
+
+        Object[] arr = coll.toArray();                //将集合转换成数组
+        for (int i = 0; i < arr.length; i++) {
+            Student s = (Student) arr[i];            //强转成Student
+            System.out.println(s.getName() + "," + s.getAge());
+        }
     }
 }
