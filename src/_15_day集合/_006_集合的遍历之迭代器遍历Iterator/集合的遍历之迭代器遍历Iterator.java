300e6fb2e0175dd63b28701e453aec855bfca805
Fongyitao <2059634680@qq.com>
2017-10-06 15:53:18 +0800

    learn

---
+++ src/_15_day集合/_006_集合的遍历之迭代器遍历Iterator/集合的遍历之迭代器遍历Iterator.java
@@ -0,0 +1,24 @@
+package _15_day集合._006_集合的遍历之迭代器遍历Iterator;
+
+public class 集合的遍历之迭代器遍历Iterator {
+    /*
+* A:迭代器概述
+    * 集合是用来存储元素,存储的元素需要查看,那么就需要迭代(遍历)
+* B:案例演示
+    * 迭代器的使用
+            Collection c = new ArrayList();
+            c.add("a");
+            c.add("b");
+            c.add("c");
+            c.add("d");
+
+            Iterator it = c.iterator();                        //获取迭代器的引用
+            while(it.hasNext()) {                            //集合中的迭代方法(遍历)
+                System.out.println(it.next());
+            }
+     */
+    public static void main(String[] args) {
+
+        
+    }
+}
