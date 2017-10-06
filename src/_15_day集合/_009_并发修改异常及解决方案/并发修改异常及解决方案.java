5dc297c4cf69da908eae71f49cfe1fe8519f0bfe
Fongyitao <2059634680@qq.com>
2017-10-06 17:20:58 +0800

    learn

--- src/_15_day集合/_009_并发修改异常及解决方案/并发修改异常及解决方案.java
+++ src/_15_day集合/_009_并发修改异常及解决方案/并发修改异常及解决方案.java
@@ -9,7 +9,14 @@ public class 并发修改异常及解决方案 {
     /*
 * A:案例演示
     * 需求：我有一个集合，请问，我想判断里面有没有"world"这个元素，如果有，我就添加一个"javaee"元素，请写代码实现。
+* B:ConcurrentModificationException出现
+    * 迭代器遍历，集合修改集合
+* C:解决方案
+    * a:迭代器迭代元素，迭代器修改元素(ListIterator的特有功能add)
+    * b:集合遍历元素，集合修改元素
+
      */
+
     public static void main(String[] args) {
         List list = new ArrayList();
         list.add("a");
@@ -30,12 +37,13 @@ public class 并发修改异常及解决方案 {
       */
 
         ListIterator lit = list.listIterator();
-       while (lit.hasNext()){
-           String str = (String)lit.next();
-           if(str.equals("world")){
-               lit.add("javaee");
-           }
-       }
+        while (lit.hasNext()){
+            String str = (String)lit.next();
+            if (str.equals("world")){
+                lit.add("javaee");
+            }
+
+        }
 
         System.out.println(list);//[a, b, world, javaee, c]
     }
---
+++ src/_15_day集合/_012_数据结构之数组和链表/数据结构之数组和链表.java
@@ -0,0 +1,17 @@
+package _15_day集合._012_数据结构之数组和链表;
+
+public class 数据结构之数组和链表 {
+    /*
+* A:数组
+    * 查询快修改也快
+    * 增删慢
+* B:链表
+    * 查询慢,修改也慢
+    * 增删快
+
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
