1cc17c0513d95487842822ee9fecedaf86fed3a9
Fongyitao <2059634680@qq.com>
2017-10-07 22:49:01 +0800

    learn

--- src/_17_day/_01_HashSet存储字符串并遍历/Person.java
+++ src/_17_day/_01_HashSet存储字符串并遍历/Person.java
@@ -35,17 +35,29 @@ public class Person {
                 '}';
     }
 
+    /*
+    健壮性判断
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) return true; // 调用的对象和传入的对象是同一个对象，直接返回true
+        if (o == null || getClass() != o.getClass()) return false; // 传入对象为null，或者，两个对象的对象对应的字节码文件是否是同一个字节码，不是就返回false
 
-        Person person = (Person) o;
+        Person person = (Person) o; //向下转型
 
-        if (age != person.age) return false;
+        if (age != person.age) return false;// 年龄不等，返回false
+
+        //调用姓名是否不为null？若不为null，判断调用对象的姓名是否等于传入对象姓名；若为null,判断传入对象姓名是否为null
         return name != null ? name.equals(person.name) : person.name == null;
     }
 
+    /*
+    为什么是31？
+    1、31是一个质数，只能被1和自己本身整除
+    2、31这个数既不大也不小
+    3、31这个数好算，2的5次方-1,2<<5 -1
+
+     */
     @Override
     public int hashCode() {
         int result = name != null ? name.hashCode() : 0;
