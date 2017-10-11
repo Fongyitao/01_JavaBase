66ac803602e30787e9f10e946cfa397ed9856bb1
Fongyitao <2059634680@qq.com>
2017-10-11 19:22:16 +0800

    learn

---
+++ src/_18_day/_012_泛型固定下边界/BaseStudent.java
@@ -0,0 +1,13 @@
+package _18_day._012_泛型固定下边界;
+
+
+import _18_day._004_HashMap集合键是Student值是String.Student;
+
+public class BaseStudent extends Student {
+    public BaseStudent() {
+    }
+
+    public BaseStudent(String name, int age) {
+        super(name, age);
+    }
+}
