b93c4d44991ff0d9af051449865da778857c9428
Fongyitao <2059634680@qq.com>
2017-10-11 22:18:56 +0800

    learn

---
+++ src/_19_day/_009_File类的概述和构造方法/_01_File类的概述和构造方法.java
@@ -0,0 +1,24 @@
+package _19_day._009_File类的概述和构造方法;
+
+public class _01_File类的概述和构造方法 {
+    /*
+    * A:File类的概述
+    * File更应该叫做一个路径
+        * 文件路径或者文件夹路径
+        * 路径分为绝对路径和相对路径
+        * 绝对路径是一个固定的路径,从盘符开始
+        * 相对路径相对于某个位置,在eclipse下是指当前项目下,在dos下
+    * 查看API指的是当前路径
+    * 文件和目录路径名的抽象表示形式
+    * B:构造方法
+        * File(String pathname)：根据一个路径得到File对象
+        * File(String parent, String child):根据一个目录和一个子文件/目录得到File对象
+        * File(File parent, String child):根据一个父File对象和一个子文件/目录得到File对象
+    * C:案例演示
+        * File类的构造方法
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
